add74716baeca1d0ccabdb77d80390026b0a7891
Feather <lisafeather@gmail.com>
2013-11-19 10:14:37 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Steps:
+
+1. create a rails app
+
+2. generate a main controller with no javascripts - this is the main index that will serve up the other pages
+
+rails g controller main index --skip-javascripts
+
+3. 
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '46d652ebd3d0af7f06bec33ec77eb5d84ad9aef8bb49bb71ed25b00e25556f0f29972608bbd101e0a83a2847811e860bc1dee3266895d988ef529356fbaa867c'
